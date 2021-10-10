7266cb241e47b2f5c7c35f708e0b700bfd43ab7a
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-10-11 00:54:11 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import requests
 from pyrogram import Client as Bot
 
 from callsmusic import run
-from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-
-response = requests.get(BG_IMAGE)
-with open("./etc/foreground.png", "wb") as file:
-    file.write(response.content)
+from config import API_ID, API_HASH, BOT_TOKEN
 
 
 bot = Bot(
@@ -14,10 +9,8 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="handlers"),
+    plugins=dict(root="handlers")
 )
-
-print("[INFO]: Rocks Music Project !")
 
 bot.start()
 run()
